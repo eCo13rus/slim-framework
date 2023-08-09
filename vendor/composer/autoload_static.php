a3ba96b4b116b7b638b204c691a2bee08a391975
Ilya Shirokov <asd.mat@mail.ru>
2023-08-09 14:17:05 +0300

    Add

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -80,8 +80,8 @@ class ComposerStaticInitba5ac1ffaab15afcc12fc818e8fcc6c2
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Container\\' => 
         array (
